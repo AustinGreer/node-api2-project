ad458bb88578b441109df2b038cba777bf0aeb94
Austin Greer <agreer.ag12@gmail.com>
2021-05-05 18:47:40 -0400

    added scaffolding for all endpoints. The endpoint for the get request by post id is working as expected.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -19,4 +19,42 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    Posts.findById(id)
+    .then(posts => {
+        if (!posts) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.json(posts)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            error: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+router.post('/', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+router.get('/:id/comments', (req, res) => {
+
+})
+
+
 module.exports = router
